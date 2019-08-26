f2afde78a14d2bbc2fdb5dbbefc1755c135f7be2
Ifechelobi Chukwuemeja <meks285@yahoo.com>
2019-08-26 14:07:27 +0100

    [#] Final Cleaning of Code snippets, Versioning.

--- omod/src/main/java/org/openmrs/module/conceptname/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/conceptname/extension/html/AdminList.java
@@ -44,7 +44,7 @@ public class AdminList extends AdministrationSectionExt {
 	 */
 	public Map<String, String> getLinks() {
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
-		map.put("/module/conceptname/manage.form", "conceptname.manage");
+		map.put("", "");
 		return map;
 	}
 	
